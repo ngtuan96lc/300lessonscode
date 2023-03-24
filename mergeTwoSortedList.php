2a136701ec59fe7a3a104d3f6b69f4bc478da638
Tuannh <ngtuan96lc@gmail.com>
2023-03-24 15:45:14 +0700

    [Lesson 21] Merge Two Sorted List - using recursive

---
+++ mergeTwoSortedList.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Solution {
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2) {
+        if ($list1 == null) {
+            return $list2;
+        }
+        if ($list2 == null) {
+            return $list1;
+        }
+
+        if ($list1->val < $list2->val) {
+            $list1->next = $this->mergeTwoLists($list1->next, $list2);
+            return $list1;
+        } else {
+            $list2->next = $this->mergeTwoLists($list2->next, $list1);
+            return $list2;
+        }
+    }
+}
